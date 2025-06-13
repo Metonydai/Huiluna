19a51cb03fd49916115d343e8c05cc8a7793c8bf
Metonydai <kusuje@gmail.com>
2025-06-13 17:04:09 +0800

    Added Renderer flow and submission

--- Huiluna/src/Huiluna/Renderer/Buffer.cpp
+++ Huiluna/src/Huiluna/Renderer/Buffer.cpp
@@ -12,8 +12,8 @@ namespace Huiluna {
 	{
 		switch (Renderer::GetAPI())
 		{
-			case RendererAPI::None:	     HL_CORE_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
-			case RendererAPI::OpenGL:	 return new OpenGLVertexBuffer(vertices, size);
+			case RendererAPI::API::None:	     HL_CORE_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
+			case RendererAPI::API::OpenGL:	     return new OpenGLVertexBuffer(vertices, size);
 		}
 
 		HL_CORE_ASSERT(false, "Unknown RendererAPI!");
@@ -24,8 +24,8 @@ namespace Huiluna {
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::None:	     HL_CORE_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
-		case RendererAPI::OpenGL:	 return new OpenGLIndexBuffer(indices, count);
+			case RendererAPI::API::None:	     HL_CORE_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
+			case RendererAPI::API::OpenGL:	     return new OpenGLIndexBuffer(indices, count);
 		}
 
 		HL_CORE_ASSERT(false, "Unknown RendererAPI!");
---
+++ Huiluna/src/Huiluna/Renderer/RendererAPI.cpp
@@ -0,0 +1,8 @@
+#include "hlpch.h"
+#include "RendererAPI.h"
+
+namespace Huiluna {
+
+	RendererAPI::API RendererAPI::s_API = RendererAPI::API::OpenGL;
+
+}
