3f831f164be66376c53f73e9d0c0f095139a217d
Metonydai <kusuje@gmail.com>
2025-07-01 10:17:20 +0800

    Stability + bug fixed

--- Huiluna/src/Platform/OpenGL/OpenGLTexture.h
+++ Huiluna/src/Platform/OpenGL/OpenGLTexture.h
@@ -20,6 +20,8 @@ namespace Huiluna {
 		virtual void SetData(void* data, uint32_t size) override;
 
 		virtual void Bind(uint32_t slot) const override;
+		
+		virtual bool IsLoaded() const override { return m_IsLoaded; }
 
 		virtual bool operator==(const Texture& other) const override
 		{ 
@@ -28,6 +30,7 @@ namespace Huiluna {
 
 	private:
 		std::string m_Path;
+		bool m_IsLoaded = false;
 
 		uint32_t m_Width, m_Height;
 		uint32_t m_RendererID;
