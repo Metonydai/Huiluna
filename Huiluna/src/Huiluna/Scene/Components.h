001e87adc52417a8e771e46c51f91e84e91f6674
Metonydai <kusuje@gmail.com>
2025-07-04 09:41:20 +0800

    CopyComponent and CopyComponentIfExists as a variadic template functions

--- Huiluna/src/Huiluna/Scene/Components.h
+++ Huiluna/src/Huiluna/Scene/Components.h
@@ -153,4 +153,14 @@ namespace Huiluna {
 		CircleCollider2DComponent(const CircleCollider2DComponent&) = default;
 	};
 
+	template<typename... Component>
+	struct ComponentGroup
+	{
+	};
+	
+	using AllComponents =
+		ComponentGroup<TransformComponent, SpriteRendererComponent,
+		CircleRendererComponent, CameraComponent, NativeScriptComponent,
+		Rigidbody2DComponent, BoxCollider2DComponent, CircleCollider2DComponent>;
+
 }
